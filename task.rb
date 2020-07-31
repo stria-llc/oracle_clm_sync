d6f1cb013935babead40039702a12cee482c3f4b
Paul Holden <paul@holdensoftware.com>
2020-07-31 09:52:48 -0700

    Fix info message for SimpleDB domain

--- task.rb
+++ task.rb
@@ -26,7 +26,7 @@ SpringCM Config:
   Data Center: #{springcm_config['datacenter']}
   Client ID: #{springcm_config['client_id']}
 AWS Config:
-  SimpleDB Delivery Log Domain: #{aws_config['simpledb']}
+  SimpleDB Delivery Log Domain: #{aws_config['simpledb_domain']}
   IAM Access Key ID: #{aws_config['access_key_id']}
     INFO
   end
